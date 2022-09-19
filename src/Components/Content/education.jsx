80f28182f7eee81ca99d8e47cf9620910220d30b
suresh-02 <suresh2002infinity@gmail.com>
2022-09-19 17:13:44 +0530

    Update education.jsx

--- src/Components/Content/education.jsx
+++ src/Components/Content/education.jsx
@@ -12,6 +12,10 @@ const Education = () => {
           <h1 className="text-[#00df9a] font-bold md:text-6xl sm:text-5xl md:py-[0] text-4xl py-[20px]">
             My Education
           </h1>
+          <p className="font-bold text-white md:text-[20px] md:mx-0 my-[20px]">
+            Bachelor of Engineering, Computer Science 2020 — 2024 Panimalar
+            Engineering College current GPA : 9.083
+          </p>
           <p className="font-bold text-white md:text-[20px] md:mx-0 my-[30px]">
             SSLC 2019 — 2020 Zion Matriculation Higher Secondary School 420/500
             (84%)
@@ -19,10 +23,6 @@ const Education = () => {
           <p className="font-bold text-white md:text-[20px] md:mx-0 my-[20px]">
             HSC 2019 — 2020 Zion Matriculation Higher Secondary School 455/600
             (75.83%)
-          </p>
-          <p className="font-bold text-white md:text-[20px] md:mx-0 my-[20px]">
-            Bachelor of Engineering, Computer Science 2020 — 2024 Panimalar
-            Engineering College current GPA : 9.083
           </p>
         </div>
       </div>
